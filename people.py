a6917affd34586b926271db2f224bcbc0b4c73fd
Mob <62955650+MobOmega@users.noreply.github.com>
2022-02-10 13:24:12 +0000

    Added class inheritance
    Other types of people inherit from base class People and added new create() function to implement defaults

--- people.py
+++ people.py
@@ -3,7 +3,11 @@ from data import *
 import string
 
 class People:
+    
     def __init__(self):
+        create(self)
+        
+    def create(self):
         self.givenName = random.choice(givenName)
         self.familyName = random.choice(familyName)
         self.sex = random.choice(['M','F'])
@@ -17,47 +21,26 @@ class People:
         self.duration = random.randint(3,30)
         self.tag = 'people'
 
-class Expired:
+        
+class Expired(People):
+    
     def __init__(self):
-        self.givenName = random.choice(givenName)
-        self.familyName = random.choice(familyName)
-        self.sex = random.choice(['M','F'])
-        self.expDate = f'{date[0]}/{date[1]}/{date[2]}'
-        self.id = ''.join(random.choices(string.ascii_uppercase, k=8))
-        self.pGivenName = self.givenName
-        self.pFamilyName = self.familyName
+        create(self)
         self.pExpDate = f'{date[0] + random.randint(1,4)}/{date[1] + random.randint(0,2)}/{date[2] + random.randint(0,1)}'
-        self.pId = self.id
-        self.reason = random.choice(['Work', 'Transit', 'Entertainment', 'Coup d\'état', 'Terrorism', 'Refugee'])
-        self.duration = random.randint(3,30)
         self.tag = 'expired'
 
-class Imposter:
+        
+class Imposter(People):
+    
     def __init__(self):
-        self.givenName = random.choice(givenName)
-        self.familyName = random.choice(familyName)
-        self.sex = random.choice(['M','F'])
-        self.expDate = f'{date[0]}/{date[1]}/{date[2]}'
-        self.id = ''.join(random.choices(string.ascii_uppercase, k=8))
+        create(self)
         self.pGivenName = imposterName[random.randint(0,len(imposterName)-1)]
-        self.pFamilyName = self.familyName
-        self.pExpDate = f'{date[0] + random.randint(1,4)}/{date[1] + random.randint(0,2)}/{date[2] + random.randint(0,1)}'
-        self.pId = self.id
-        self.reason = random.choice(['Work', 'Transit', 'Entertainment', 'Coup d\'état', 'Terrorism', 'Refugee'])
-        self.duration = random.randint(3,30)
         self.tag = 'imposter'
 
-class Identity:
+        
+class Identity(People):
+    
     def __init__(self):
-        self.givenName = random.choice(givenName)
-        self.familyName = random.choice(familyName)
-        self.sex = random.choice(['M','F'])
-        self.expDate = f'{date[0]}/{date[1]}/{date[2]}'
-        self.id = ''.join(random.choices(string.ascii_uppercase, k=8))
-        self.pGivenName = self.givenName
-        self.pFamilyName = self.familyName
-        self.pExpDate = f'{date[0] + random.randint(1,4)}/{date[1] + random.randint(0,2)}/{date[2] + random.randint(0,1)}'
+        create(self)
         self.pId = ''.join(random.choices(string.ascii_uppercase, k=8))
-        self.reason = random.choice(['Work', 'Transit', 'Entertainment', 'Coup d\'état', 'Terrorism', 'Refugee'])
-        self.duration = random.randint(3,30)
         self.tag = 'identity'
